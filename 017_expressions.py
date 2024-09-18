a74ea57e339b3413d1953dd5e52f70004a51d51d
Baber Khan <bbrkhnn@outlook.com>
2024-09-18 13:14:44 +0100

    import from replit repo

--- 017_expressions.py
+++ 017_expressions.py
@@ -18,10 +18,10 @@ added = 2 + 3
 
 # Here are some more examples of expressions:
 
-2            # Evaluates to: 2
-2 + 3        # Evaluates to: 5
-2 * 3        # Evaluates to: 6
-2 + 3 * 4    # Evaluates to: ...???
+2  # Evaluates to: 2
+2 + 3  # Evaluates to: 5
+2 * 3  # Evaluates to: 6
+2 + 3 * 4  # Evaluates to: 14
 
 # Well, what is that last one?
 #
@@ -65,14 +65,16 @@ added = 2 + 3
 
 # I'm going to reintroduce the `add_one` function:
 
+
 def add_one(num):
   return num + 1
 
+
 # Here are some more expressions:
 
-add_one(2)          # Evaluates to 3
-add_one(3)          # Evaluates to 4
-add_one(4) * 3      # Evaluates to 15
+add_one(2)  # Evaluates to 3
+add_one(3)  # Evaluates to 4
+add_one(4) * 3  # Evaluates to 15
 2 + add_one(4) * 3  # Evaluates to 17
 
 # What to take from this? Calling a function is also an
@@ -101,7 +103,7 @@ add_one(add_one(add_one(add_one(add_one(add_one(1))))))
 
 # EXPRESSIONS PLAYGROUND BEGINS
 
-
+add_one(10) - 1 * 5  # evaluates to 6
 
 # EXPRESSIONS PLAYGROUND ENDS
 
